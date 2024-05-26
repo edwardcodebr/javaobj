2e9475ffd262785d0b8e21ec0b20e330d6fa26a7
Carlos Eduardo Correa Queiroz <156525508+edwardcodebr@users.noreply.github.com>
2024-05-26 16:30:53 -0400

    java jocking
    jocking in java messages

---
+++ app.java
@@ -0,0 +1,10 @@
+
+public class app{
+    public static void main(String[] args) {
+        int a = 1;
+        for(int i = 0 ; i < 100 ; i ++){
+            System.out.printf("%d - me desculpa amor.\n", +a);
+            a++;
+        }
+    }
+}
